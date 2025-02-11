f298e155c040da055d54e9b29251889b149514a8
subhra <roys9964@gmail.com>
2025-02-11 22:33:08 +0530

    updated

--- edit-js/edit-js.js
+++ edit-js/edit-js.js
@@ -1,6 +1,54 @@
 const header = document.querySelector("header");
 const times = header.querySelector(".times");
 const bars = header.querySelector(".bars");
+const dropdown = document.querySelector(".dropdown-link");
+const subMenu = document.querySelector(".sub-menu");
+const inputBoxs = document.querySelectorAll(".input-box");
+
+inputBoxs.forEach((input) => {
+  const inputLabel = input.querySelector("label");
+  const inputField = input.querySelector("input");
+  console.log(inputField.value);
+  input.addEventListener("input", (e) => {
+    inputLabel.classList.add("show");
+    if (inputField.value == "") {
+      inputLabel.classList.remove("show");
+    } else {
+      inputLabel.classList.add("show");
+    }
+    inputBoxs.forEach((input2) => {
+      if (input != input2) {
+        const inputLabel2 = input2.querySelector("label");
+        const inputField2 = input2.querySelector("input");
+        inputLabel2.classList.remove("show");
+        if (inputField2.value == "") {
+          inputLabel2.classList.remove("show");
+        } else {
+          inputLabel2.classList.add("show");
+        }
+      }
+    });
+  });
+});
+
+// window.addEventListener("mouseup", (e) => {
+//   inputBoxs.forEach((input) => {
+//     const inputLabel = input.querySelector("label");
+//     const inputField = input.querySelector("input");
+
+//     if (!input.contains(e.target)) {
+//       if (inputField.value == "") {
+//         inputLabel.classList.remove("show");
+//       } else {
+//         inputLabel.classList.add("show");
+//       }
+//     }
+//   });
+// });
+
+dropdown.addEventListener("click", (e) => {
+  subMenu.classList.toggle("active");
+});
 
 bars.addEventListener("click", (e) => {
   header.classList.add("show");
@@ -87,18 +135,175 @@ var swiper = new Swiper(".swiper-testi", {
   },
 });
 
+var swiper = new Swiper(".swiper-hero", {
+  loop: true,
+  effect: "fade",
+  spaceBetween: 0,
+  grabCursor: true,
+  slidesPerView: 1,
+  loop: true,
+  autoplay: {
+    delay: 3000,
+  },
+  // autoHeight: true,
+  // height: 200,
+  // navigation: {
+  //   nextEl: ".swiper-button-next",
+  //   prevEl: ".swiper-button-prev",
+  // },
+  pagination: {
+    el: ".swiper-paginationn",
+    clickable: true,
+  },
+  speed: 1000,
+});
+
+var swiper = new Swiper(".swiper-offer", {
+  loop: true,
+  spaceBetween: 20,
+  grabCursor: true,
+  slidesPerView: 1,
+  loop: true,
+  autoplay: {
+    delay: 3000,
+  },
+  autoHeight: true,
+  height: 200,
+  // navigation: {
+  //   nextEl: ".swiper-button-next",
+  //   prevEl: ".swiper-button-prev",
+  // },
+  pagination: {
+    el: ".swiper-paginationnn",
+    clickable: true,
+  },
+  speed: 1000,
+});
+
+var swiper = new Swiper(".swiper-blog", {
+  loop: true,
+  spaceBetween: 20,
+  grabCursor: true,
+  slidesPerView: 1,
+  loop: true,
+  autoplay: {
+    delay: 3000,
+  },
+  autoHeight: true,
+  height: 200,
+  // navigation: {
+  //   nextEl: ".swiper-button-next",
+  //   prevEl: ".swiper-button-prev",
+  // },
+  pagination: {
+    el: ".swiper-pagination-blog",
+    clickable: true,
+  },
+  speed: 1000,
+});
+
+var swiper = new Swiper(".swiper-blog2", {
+  loop: true,
+  spaceBetween: 20,
+  grabCursor: true,
+  slidesPerView: 1,
+  loop: true,
+  // autoplay: {
+  //   delay: 3000,
+  // },
+  autoHeight: true,
+  height: 200,
+  navigation: {
+    nextEl: ".single-swiper-btn-next",
+    prevEl: ".single-swiper-btn-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination-blog",
+    clickable: true,
+  },
+  speed: 1000,
+});
+
 function equalizeCardHeights() {
   var cards = document.querySelectorAll(".testi-content");
   var maxHeight = 0;
 
   cards.forEach(function (card) {
-    card.style.height = "auto"; // Reset card heights first
+    card.style.height = "auto";
     var cardHeight = card.offsetHeight;
     maxHeight = Math.max(maxHeight, cardHeight);
   });
 
-  // Set all card heights to the maximum height found
   cards.forEach(function (card) {
     card.style.height = maxHeight + "px";
   });
 }
+
+$(function () {
+  let visibleBoxes = 6;
+  const boxes = document.querySelectorAll(".blog-col");
+  const loadMoreBtn = document.querySelector(".load-more-btn");
+
+  function showInitialBoxes() {
+    for (let i = 0; i < visibleBoxes; i++) {
+      if (boxes[i]) {
+        boxes[i].classList.add("show");
+      }
+    }
+  }
+
+  function loadMore() {
+    let newVisibleCount = visibleBoxes + 6;
+    for (let i = visibleBoxes; i < newVisibleCount; i++) {
+      if (boxes[i]) {
+        boxes[i].classList.add("show");
+      }
+    }
+    visibleBoxes = newVisibleCount;
+
+    if (visibleBoxes >= boxes.length) {
+      loadMoreBtn.style.display = "none";
+    }
+  }
+
+  loadMoreBtn.addEventListener("click", loadMore);
+
+  showInitialBoxes();
+});
+
+(function () {
+  const priceAccs = document.querySelectorAll(".pricing-acc");
+  const pricingBtmOuters = document.querySelectorAll(".pricing-btm-outer");
+  const icons = document.querySelectorAll(".icon span");
+  pricingBtmOuters[0].style.maxHeight = pricingBtmOuters[0].scrollHeight + "px";
+  icons[0].textContent = "-";
+
+  priceAccs.forEach((price) => {
+    const priceTop = price.querySelector(".pricing-acc-top");
+    const priceBtm = price.querySelector(".pricing-btm-outer");
+    const icon = price.querySelector(".icon span");
+    priceTop.addEventListener("click", (e) => {
+      price.classList.toggle("active");
+      if (priceBtm.style.maxHeight) {
+        priceBtm.style.maxHeight = null;
+        icon.textContent = "+";
+      } else {
+        priceBtm.style.maxHeight = priceBtm.scrollHeight + "px";
+        icon.textContent = "-";
+      }
+
+      priceAccs.forEach((price2) => {
+        if (price != price2) {
+          const priceTop2 = price2.querySelector(".pricing-acc-top");
+          const priceBtm2 = price2.querySelector(".pricing-btm-outer");
+          const icon2 = price2.querySelector(".icon span");
+
+          priceBtm2.style.maxHeight = null;
+          icon2.textContent = "+";
+        }
+      });
+    });
+  });
+})();
+
+$(function () {});
